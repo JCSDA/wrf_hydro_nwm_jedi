07c07ecf6694a61bc58db74e0bc30e26055e0615
afanfa <alessandro.fanfarillo@gmail.com>
2020-02-06 14:52:51 -0700

    Implementing Geometry with new names

---
+++ src/wrf_hydro_nwm_jedi/Geometry/Geometry.cc
@@ -0,0 +1,75 @@
+/*
+ * (C) Copyright 2019-2019 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "wrf_hydro_nwm_jedi/Geometry/Geometry.h"
+// #include "wrf_hydro_nwm_jedi/GeometryIterator/GeometryIterator.h"
+#include "wrf_hydro_nwm_jedi/Geometry/GeometryFortran.h"
+
+#include "eckit/config/Configuration.h"
+
+#include "oops/util/abor1_cpp.h"
+
+namespace wrf_hydro_nwm_jedi {
+
+// ----------------------------------------------------------------------------
+
+  Geometry::Geometry(const eckit::Configuration & conf,
+                     const eckit::mpi::Comm & comm)
+    : comm_(comm) {
+    const eckit::Configuration * configc = &conf;
+    // std::cout << "Geometry: "
+    //    << "(TODO, print diagnostic info about the geometry here)"
+    //    << std::endl;
+    wrf_hydro_nwm_jedi_geometry_setup_f90(keyGeom_, &configc);
+    print(std::cout);
+  }
+
+// ----------------------------------------------------------------------------
+
+  Geometry::Geometry(const Geometry & other)
+    : comm_(other.comm_) {
+    wrf_hydro_nwm_jedi_geometry_clone_f90(keyGeom_, other.keyGeom_);
+  }
+
+// ----------------------------------------------------------------------------
+
+  Geometry::~Geometry() {
+    wrf_hydro_nwm_jedi_geometry_delete_f90(keyGeom_);
+  }
+
+// ----------------------------------------------------------------------------
+
+  void Geometry::print(std::ostream & os) const {
+    float dx,dy;
+    // util::abor1_cpp("Geometry::print() needs to be implemented.",
+    //                 __FILE__, __LINE__);
+    os << "Geometry: "
+       << "(TODO, print diagnostic info about the geometry here)"
+       << std::endl;
+    wrf_hydro_nwm_jedi_geometry_info_f90 (keyGeom_, &dx, &dy);
+    os << "dx = " << dx << ", dy = " << dy << std::endl;
+  }
+
+// ----------------------------------------------------------------------------
+
+  // GeometryIterator Geometry::begin() const {
+  //   // util::abor1_cpp("Geometry::begin() needs to be implemented.",
+  //   //                 __FILE__, __LINE__);
+  //   return GeometryIterator();
+  // }
+
+// ----------------------------------------------------------------------------
+
+  // GeometryIterator Geometry::end() const {
+  //   // util::abor1_cpp("Geometry::end() needs to be implemented.",
+  //   //                 __FILE__, __LINE__);
+  //   return GeometryIterator();
+  // }
+
+// ----------------------------------------------------------------------------
+
+}  // namespace wrf_hydro_nwm_jedi
