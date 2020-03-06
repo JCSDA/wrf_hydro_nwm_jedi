3af2cbcc542327644ae3d562f26fd786da9fc39f
afanfa <elfanfa@ucar.edu>
2020-03-06 11:07:56 -0700

    New values in Geometry, basic version

--- src/wrf_hydro_nwm_jedi/Geometry/Geometry.F90
+++ src/wrf_hydro_nwm_jedi/Geometry/Geometry.F90
@@ -11,9 +11,10 @@ public :: wrf_hydro_nwm_jedi_geometry
 !------------------------------------------------------------------------------
 
 type :: wrf_hydro_nwm_jedi_geometry
-   integer :: ix, iy
-   integer :: xstart, ystart
-   integer :: xend, yend
+   integer :: ix, iy !unsused
+   integer :: xstart, ystart !unsused
+   integer :: xend, yend !unsused
+   integer :: npx, npy, npz
    real :: dx, dy
  contains
    procedure :: init   => wrf_hydro_nwm_jedi_geometry_init
@@ -35,7 +36,6 @@ subroutine wrf_hydro_nwm_jedi_geometry_init(self, f_conf)
   character(len=:), allocatable :: str
   integer :: ierr,ncid
 
-  !wrfinput_flnm = '/home/alex/Downloads/JEDI/src/build/wrf_hydro_jedi/test/testinput/wrfinput.nc'
   call f_conf%get_or_die("input_file",str)
   wrfinput_flnm = str
   write(*,*) 'Input file for Geometry: ',wrfinput_flnm
@@ -53,15 +53,15 @@ subroutine wrf_hydro_nwm_jedi_geometry_init(self, f_conf)
   end if
 
   ierr = nf90_get_att(ncid, NF90_GLOBAL, "DX", self%dx)
-
   ierr = nf90_get_att(ncid, NF90_GLOBAL, "DY", self%dy)
-
+  ierr = nf90_get_att(ncid, NF90_GLOBAL, "west_east", self%npx)
+  ierr = nf90_get_att(ncid, NF90_GLOBAL, "south_north", self%npy)
+  ierr = nf90_get_att(ncid, NF90_GLOBAL, "soil_layers_stag", self%npz)
+  
   if(ierr /= 0) then
      write(*,*) ierr
      call abor1_ftn("ERROR: dx and/or dy need by Geometry not found")
   end if
-
-  write(*,*) "Dx and dy from Fortran", self%dx, self%dy
   
 end subroutine
 
@@ -70,26 +70,34 @@ end subroutine
 subroutine wrf_hydro_nwm_jedi_geometry_clone(self, other)
   class(wrf_hydro_nwm_jedi_geometry),  intent(out) :: self
   class(wrf_hydro_nwm_jedi_geometry),   intent(in) :: other
-      
-  ! call abor1_ftn("ERROR: wrf_hydro_nwm_jedi_geometry_clone() needs to be implemented.")
+
+  self%dx = other%dx
+  self%dy = other%dy
+  self%npx = other%npx
+  self%npy = other%npy
+  self%npz = other%npz
+  
 end subroutine
   
 !------------------------------------------------------------------------------
 
 subroutine wrf_hydro_nwm_jedi_geometry_delete(self)
   class(wrf_hydro_nwm_jedi_geometry),  intent(out) :: self
-       
-  ! call abor1_ftn("ERROR: wrf_hydro_nwm_jedi_geometry_delete() needs to be implemented.")
+
 end subroutine
 
 !------------------------------------------------------------------------------
 
-subroutine wrf_hydro_nwm_jedi_geometry_get_info(self,dx_,dy_)
+subroutine wrf_hydro_nwm_jedi_geometry_get_info(self,dx_,dy_,npx_,npy_,npz_)
   class(wrf_hydro_nwm_jedi_geometry),  intent(in) :: self
   real, intent(out) :: dx_,dy_
+  integer, intent(out) :: npx_,npy_,npz_
 
   dx_ = self%dx
   dy_ = self%dy
+  npx_ = self%npx
+  npy_ = self%npy
+  npz_ = self%npz  
 
 end subroutine wrf_hydro_nwm_jedi_geometry_get_info
 
