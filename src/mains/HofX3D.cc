f205c31c60ad55152a38b981681f6739efa35cf9
amfox37 <amfox37@gmail.com>
2021-05-10 22:56:48 +0000

    removing hofxslow

---
+++ src/mains/HofX3D.cc
@@ -0,0 +1,21 @@
+/*
+ * (C) Copyright 2019-2019 UCAR.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "oops/runs/HofX3D.h"
+#include "oops/runs/Run.h"
+#include "ufo/instantiateObsFilterFactory.h"
+#include "ufo/ObsTraits.h"
+
+#include "wrf_hydro_nwm_jedi/Traits.h"
+
+
+int main(int argc,  char ** argv) {
+  oops::Run run(argc, argv);
+  ufo::instantiateObsFilterFactory<ufo::ObsTraits>();
+  oops::HofX3Dslow<wrf_hydro_nwm_jedi::Traits, ufo::ObsTraits> hofx;
+  return run.execute(hofx);
+}
